2f712a8b9400e575c1812cbc8860eefea2af92ff
Ahmed Shahzad <243330@students.au.edu.pk>
2026-08-09 21:20:44 +0500

    Admin page auth context addition

--- admin/src/api/client.js
+++ admin/src/api/client.js
@@ -4,4 +4,21 @@ const api = axios.create({
   baseURL: 'http://localhost:4000/api',
 })
 
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('admin_token')
+  if (token) config.headers.Authorization = `Bearer ${token}`
+  return config
+})
+
+api.interceptors.response.use(
+  (res) => res,
+  (err) => {
+    if (err.response?.status === 401) {
+      localStorage.removeItem('admin_token')
+      window.location.href = '/login'
+    }
+    return Promise.reject(err)
+  }
+)
+
 export default api
